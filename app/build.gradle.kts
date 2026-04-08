fa44bc00ff72c4d898c4446539464f069e8e820f
kunvarpreet15 <kunvarpreet15@gmail.com>
2026-04-08 15:04:38 +0530

    chore: Optimize dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.autonomousapps.dependency.analysis)
 }
+//noinspection OldTargetApi
 android {
     namespace = "com.kunvarpreet.to_dolist"
     compileSdk {
@@ -40,32 +42,23 @@ android {
     }
 }
 dependencies {
-    implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.androidx.compose.material.icons.core)
-    implementation(libs.androidx.compose.material)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.compose.animation)
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.androidx.compose.runtime)
-    implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.ui.text)
-    implementation(libs.androidx.ui)
     ksp(libs.androidx.room.compiler)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.text)
+    implementation(libs.androidx.room.runtime)
+    implementation(platform(libs.androidx.compose.bom))
+    runtimeOnly(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugRuntimeOnly(libs.androidx.compose.ui.test.manifest)
 }
